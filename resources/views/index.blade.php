768b000b1df5c99348fd542f0c80e8a01935ad9c
Szeto <jokersk119@gmail.com>
2024-06-25 16:32:00 +0800

    wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -132,7 +132,8 @@
                             @endforeach
                             @if ($actions)
                                 <x-tables::cell>
-                                    <div class="filament-tables-actions-cell whitespace-nowrap px-4 py-3">
+                                    <div
+                                        class="filament-tables-actions-cell whitespace-nowrap px-4 py-3 flex justify-end space-x-2">
                                         @foreach($actions as $action)
                                             @php
                                                 $action->record($record)->setKey( str(data_get($record, $keyBy))->slug()->value()  );
